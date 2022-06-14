9c13e850c7a007339e44309d4ed21df0610b55b5
m <mark@lovink.ch>
2022-06-14 10:42:39 +0200

    Comments menu + better option

--- Labyrinthe Complex/Menu.cs
+++ Labyrinthe Complex/Menu.cs
@@ -1,33 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//Author            : Mark Lovink
+//Date              : 14.06.2022
+//Company           : Etml, Lausanne
+//Description       : Classe qui s'occupe du menu du jeu
+
+using System;
 using static System.Console;
 
 
 namespace Labyrinthe_Complex
 {
-    
-   
+
+    /// <summary>
+    /// Menu avec la hauteur et longeur du labyrinthe et lance l'affichage des labyrinthe
+    /// </summary>
     public class Menu
     {
-        //La liste de tous les robots
-                                                           // private bool _difficutly;//Variable qui stoque l'état de l'option difficulté   
+        /// <summary>
+        /// Méthode qui affiche le menu du jeu, Titre, Jouer, Résolution, Exit
+        /// </summary>
         public void StartGameSettings()
         {
             Clear();
-            SetWindowSize(80,30);
-            CursorVisible = false;
-            int topPos = 16;
-            int startLeftPos = 4;
-            int userChoice = 0;
-            int userLabWidth = 0;
-            int userLabHeigth = 0;
-            ConsoleKeyInfo keyInfo;
-
-            ForegroundColor = ConsoleColor.Yellow;
-            Write(@"  
+            SetWindowSize(80, 30);//Taille de la console
+            CursorVisible = false; // Curseur invisible
+            int topPos = 16;//Position de départ par rapport au haut de la console
+            int startLeftPos = 4;//Position de départ par rapport à la gauche de la console
+            int userChoice = 0;//Choix du joueur
+            int userLabWidth = 0;//Longeur du labyrinthe
+            int userLabHeigth = 0;//Hauteur du labyrinthe
+            ConsoleKeyInfo keyInfo;//Info sur la touche utilisée
+            do
+            {
+                Clear();
+                ForegroundColor = ConsoleColor.Yellow;
+                //Titre du programme
+                WriteLine(@"  
     _                _                      _           _     _            
    | |              | |                    (_)         | |   | |           
    | |        __ _  | |__    _   _   _ __   _   _ __   | |_  | |__     ___ 
@@ -37,6 +44,35 @@ namespace Labyrinthe_Complex
                               __/ |                                        
                              |___/           ");
 
+
+
+                ResetColor();
+                SetCursorPosition(0, 12);
+
+                //Récupère la longeur et hauteur que le joueur veut
+                try
+                {
+                    Write("Longeur du labyrinthe");
+                    ForegroundColor = ConsoleColor.Red;
+                    Write(" (chiffre impair plus petit que 80) : ");
+                    ResetColor();
+                    userLabWidth = int.Parse(ReadLine());
+                    Write("Hauteur du labyrinthe");
+                    ForegroundColor = ConsoleColor.Red;
+                    Write(" (chiffre impair plus petit que 58) : ");
+                    ResetColor();
+                    userLabHeigth = int.Parse(ReadLine());
+                }
+                //Si la valeur donnée retourne une erreur
+                catch
+                {
+                    Clear();
+                 
+                }
+                //Tant que les valeurs ne sont pas les bonnes
+            } while (userLabWidth > 80 || userLabHeigth > 58 || userLabWidth % 2 != 1 || userLabHeigth % 2 != 1);
+
+            //Affiche les options
             SetCursorPosition(1, topPos);
             ForegroundColor = ConsoleColor.Cyan;
             Write(">> ");
@@ -47,7 +83,16 @@ namespace Labyrinthe_Complex
             WriteLine("Résolution");
             SetCursorPosition(startLeftPos, 18);
             WriteLine("Exit");
-         
+
+
+
+            int[,] labyrinthIni = new int[userLabWidth, userLabHeigth]; // Tableau à 2 dimensions du labyrinthe initial
+            int[,] labyrinthClose = new int[userLabWidth, userLabHeigth]; // Tableau à 2 dimensions du labyrinthe avec un parcours aléatoirement construit
+            bool[,] passed = new bool[userLabWidth, userLabHeigth];// // Tableau à 2 dimensions du labyrinthe avec le parcours du joueur
+            int[,] reso = new int[userLabWidth, userLabWidth]; // Tableau à 2 dimensions du labyrinthe avec la solution du parcours le plus court
+
+
+
 
             //Déplace la flèche dans le menu du jeu
             do
@@ -94,33 +139,8 @@ namespace Labyrinthe_Complex
             {
                 //Lance le jeu
                 case 16:
-                   
-                    do
-                    {
-                        Clear();
-                        try
-                        {
-                            Write("Longeur du labyrinthe");
-                            ForegroundColor = ConsoleColor.Red;
-                            Write(" (chiffre impair plus petit que 80) : ");
-                            ResetColor();
-                            userLabWidth = int.Parse(ReadLine());
-                            Write("Hauteur du labyrinthe");
-                            ForegroundColor = ConsoleColor.Red;
-                            Write(" (chiffre impair plus petit que 58) : ");
-                            ResetColor();
-                            userLabHeigth = int.Parse(ReadLine());
-                        }
-                        catch
-                        {
-                            WriteLine("Merci donner un valeure correcte ! ");
-                        }
-                    } while (userLabWidth > 80 || userLabHeigth > 58 || userLabWidth%2!=1 || userLabHeigth % 2 != 1);
-                    int[,] labyrinthIni = new int[userLabWidth, userLabHeigth]; // Tableau à 2 dimensions du labyrinthe initial
-                    int[,] labyrinthClose = new int[userLabWidth, userLabHeigth]; // Tableau à 2 dimensions du labyrinthe avec un parcours aléatoirement construit
-                    int[,] reso = new int[0, 0]; // Tableau à 2 dimensions du labyrinthe avec la solution du parcours le plus court
-                    bool[,] passed = new bool[userLabWidth, userLabHeigth];
-                    Labyrinthe lab = new Labyrinthe(labyrinthIni, labyrinthClose, reso, passed);
+                    Clear();
+                    Labyrinthe lab = new Labyrinthe(labyrinthIni, labyrinthClose, passed);
                     lab.Show();
                     lab.Play();
                     StartGameSettings();
@@ -128,31 +148,7 @@ namespace Labyrinthe_Complex
                 //Lance la résolution
                 case 17:
                     Clear();
-                    do
-                    {
-                        try
-                        {
-                            Write("Longeur du labyrinthe");
-                            ForegroundColor = ConsoleColor.Red;
-                            Write(" (chiffre impair plus petit que 80) : ");
-                            ResetColor();
-                            userLabWidth = int.Parse(ReadLine());
-                            Write("Hauteur du labyrinthe");
-                            ForegroundColor = ConsoleColor.Red;
-                            Write(" (chiffre impair plus petit que 58) : ");
-                            ResetColor();
-                            userLabHeigth = int.Parse(ReadLine());
-                        }
-                        catch
-                        {
-                            WriteLine("Merci donner un valeure correcte ! ");
-                        }
-                    } while (userLabWidth > 80 || userLabHeigth > 58 || userLabWidth % 2 != 1 || userLabHeigth % 2 != 1);
-                    labyrinthIni = new int[userLabWidth, userLabHeigth]; // Tableau à 2 dimensions du labyrinthe initial
-                    labyrinthClose = new int[userLabWidth, userLabHeigth]; // Tableau à 2 dimensions du labyrinthe avec un parcours aléatoirement construit
-                    reso = new int[userLabWidth, userLabHeigth]; // Tableau à 2 dimensions du labyrinthe avec la solution du parcours le plus court
-                    passed = new bool[0, 0];
-                    Labyrinthe labRe = new Labyrinthe(labyrinthIni, labyrinthClose, reso, passed);
+                    Labyrinthe labRe = new Labyrinthe(labyrinthIni, labyrinthClose, reso);
                     labRe.Show();
                     labRe.Reso();
                     ReadKey();
@@ -162,10 +158,10 @@ namespace Labyrinthe_Complex
                 case 18:
                     Environment.Exit(0);
                     break;
-               
 
-              
-        
+
+
+
 
 
 
@@ -173,104 +169,8 @@ namespace Labyrinthe_Complex
             }
 
         }
-        //public void Options()
-        //{
-        //    ConsoleKeyInfo keyInfo;
-          
-        //    string difficulty = "";
-           
 
-        //    if (!_difficutly)
-        //    {
-        //        difficulty = "    Facile";
-        //    }
-        //    else if (_difficutly)
-        //    {
-        //        difficulty = " Difficile";
-        //    }
-
-        //    int topPos = 4;
-        //    int userChoice = 0;
-
-
-        //    Clear();
-
-        //    WriteLine("Option");
-        //    WriteLine("--------------------");
-        //    SetCursorPosition(1, topPos);
-        //    ForegroundColor = ConsoleColor.Cyan;
-        //    Write(">> ");
-        //    ResetColor();
-        //    SetCursorPosition(4, topPos );
-        //    WriteLine("Difficulté " + difficulty);
-        //    SetCursorPosition(4, topPos + 2);
-        //    WriteLine("Retour ");
-
-        //    do
-        //    {  //Déplace la flèche dans le menu des options
-        //        do
-        //        {
-        //            keyInfo = ReadKey(true);
-
-        //            if (keyInfo.Key == ConsoleKey.DownArrow )
-        //            {
-
-        //                MoveBufferArea(1, topPos, 2, 2, 1, topPos + 2);
-        //                topPos += 2;
-        //                if(topPos >6)
-        //                {
-        //                    MoveBufferArea(1, topPos, 2, 2, 1, topPos -4);
-        //                    topPos -= 4;
-        //                }
-
-        //            }
-        //            else if (keyInfo.Key == ConsoleKey.UpArrow )
-        //            {
-        //                MoveBufferArea(1, topPos, 2, 2, 1, topPos - 2);
-        //                topPos -= 2;
-        //                if(topPos<4)
-        //                {
-        //                    MoveBufferArea(1, topPos, 2, 2, 1, topPos + 4);
-        //                    topPos += 4;
-        //                }
-        //            }
-        //            else if (keyInfo.Key == ConsoleKey.Enter)
-        //            {
-        //                userChoice = topPos - 4;
-        //            }
-
-        //        }
-        //        while (keyInfo.Key != ConsoleKey.Enter);
-
-        //        //Change les options de son et de difficulté 
-        //        switch (userChoice)
-        //        {
-
-        //            case 0:
-        //                _difficutly = !_difficutly;
-        //                if (!_difficutly)
-        //                {
-        //                    difficulty = "    Facile";
-        //                }
-        //                else if (_difficutly)
-        //                {
-        //                    difficulty = " Difficile";
-        //                }
-        //                SetCursorPosition(4, topPos);
-        //                WriteLine("Difficulté " + difficulty);
-        //                break;
-        //            case 2:
-        //                StartGameSettings();
-        //                break;
-
-
-
-
-        //        }
-        //    } while (userChoice != 2);
-
-        //}
 
     }
-        
+
 }
